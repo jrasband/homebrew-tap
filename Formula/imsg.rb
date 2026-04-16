9ceb9f50b1740bbb1fdef7f1f5327b9bb244cd2f
Joshua Rasband <joshuarasband@Joshuas-Laptop.local>
2026-04-16 17:09:55 -0500

    Update imsg formula

--- Formula/imsg.rb
+++ Formula/imsg.rb
@@ -1,8 +1,9 @@
 class Imsg < Formula
   desc "Read iMessages from the command line"
   homepage "https://github.com/jrasband/imsg"
-  url "https://github.com/jrasband/imsg/archive/refs/heads/main.tar.gz"
-  sha256 "adaf7c1eff94dd4e06db81f144a514bb1ee07767bb9e27360b7e1dfa7c2c392f"
+  url "https://github.com/jrasband/imsg/archive/refs/tags/v0.5.0.tar.gz"
+  sha256 "d5558cd419c8d46bdc958064cb97f963d1ea793866414c025906ec15033512ed"
+  version: "0.5.0"
   license "MIT"
 
   depends_on xcode: ["13.0", :build]
